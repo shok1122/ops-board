903178519cb818706ed52d050f7c243e25e9a0fe
Shohei Kakei <kakei.shohei@nitech.ac.jp>
2026-04-16 09:54:48 +0000

    証明書チェックのロジックを修正

--- backend/app/scripts/monitors/ssl_cert_expiry_days.sh
+++ backend/app/scripts/monitors/ssl_cert_expiry_days.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
 # @label SSL Cert Expiry
-# @unit 日
+# @unit %
 # @configurable true
 # @config_field key=port label="HTTPS port" default=443
-echo | openssl s_client -connect {host}:{port} -servername {host} 2>/dev/null | openssl x509 -noout -enddate | awk -F= '{cmd="date -d \""$2"\" +%s"; cmd | getline exp; close(cmd); print int((exp-systime())/86400)}'
+
+ssl_output=$(echo | openssl s_client -connect www.ssn.nitech.ac.jp:443 2>/dev/null | openssl x509 -noout -startdate -enddate)
+start_date=$(echo "$ssl_output" | grep notBefore | cut -d= -f2-)
+end_date=$(echo "$ssl_output"   | grep notAfter  | cut -d= -f2-)
+
+echo "Start date: $start_date"
+echo "End date: $end_date"
+
+start_exp=$(date -d "$start_date" +%s)
+end_exp=$(date -d "$end_date" +%s)
+now=$(date +%s)
+
+echo $(( ((now - start_exp) * 100) / (end_exp - start_exp) ))
